2be525b0331f22fa80cc1c174c02c6059eb21c30
Ángel Cruz García <angi_ed01@hotmail.com>
2022-02-01 00:04:06 -0600

    Third Commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          El damiany no se baña.
+          El damiany si se baña.
         </p>
         <a
           className="App-link"
